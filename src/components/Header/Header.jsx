5fb5beb35c72716901b55e3ec786564017f55e83
DebMishra <debmishra23@gmail.com>
2026-01-07 11:56:29 +0530

    Day 2: CSS Grid and Flexbox responsive layout

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <h2 className="logo">Business Website</h2>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
